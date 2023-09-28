22675bdd182ba1ee81155caa582ed73169cc7b3d
Tuedo <tuedoduc1111@gmail.com>
2023-09-29 01:09:05 +0700

    update name

---
+++ C# Basic/Uperandlower.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string txt = "Do Duc Tue 2121050190";
+      Console.WriteLine(txt.ToUpper());   
+      Console.WriteLine(txt.ToLower());  
+    }
+  }
+}
