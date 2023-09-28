3d4faf4b4915601937b84572d946a5090d671459
Tuedo <tuedoduc1111@gmail.com>
2023-09-29 01:18:16 +0700

    update vi tri

---
+++ C# Basic/accesss.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string myString = "Do Duc Tue";
+      Console.WriteLine(myString[0]);
+    }
+  }
+}
