08dbaf91e2cf56d612253581d350daae3bcae813
Tuedo <tuedoduc1111@gmail.com>
2023-09-28 17:55:24 +0700

    update

---
+++ NewApp/models/Student.cs
@@ -0,0 +1,42 @@
+using System;
+namespace NewApp.Models
+{
+    public class Student
+    {
+        public string StudentID { get; set; }   
+        public string FullName  { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        public void Input()
+        {
+            try{
+                System.Console.Write("Student ID : ");
+                StudentID = Console.ReadLine();
+            }catch(Exception ex)
+            {
+                System.Console.WriteLine("Co ngoai le xay ra!");
+            }
+            
+            System.Console.Write("FullNam : ");
+            FullName = Console.ReadLine();
+            
+            System.Console.Write("Address : ");
+            Address = Console.ReadLine();
+            
+            try{
+                System.Console.Write("Age : ");
+                Age = Convert.ToInt32(Console.ReadLine());
+            }catch(Exception ex)
+            {
+                System.Console.WriteLine("co ngoai le xay ra !");
+            }
+            
+        }
+
+        public void Output()
+        {
+            System.Console.WriteLine(StudentID +"-"+ FullName +"-"+ Address +"-"+ Age +"Tuoi");
+        }
+    }
+}
