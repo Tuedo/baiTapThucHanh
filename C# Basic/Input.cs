b4afe4b0cb1ceefb3b7879b409a9dd2a1139d740
Tuedo <tuedoduc1111@gmail.com>
2023-09-29 00:55:55 +0700

    update input

---
+++ C# Basic/Input.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      Console.WriteLine("Nhập tên của bạn:");
+
+      
+      string userName = Console.ReadLine();
+
+      
+      Console.WriteLine("Tên của bạn là: " + userName);
+    }
+  }
+}
