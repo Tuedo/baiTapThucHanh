062b4ba61c4829366395a3e5f22abecc71dadf35
Tuedo <tuedoduc1111@gmail.com>
2023-09-29 00:52:36 +0700

    update

---
+++ C# Basic/Variables.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Do Duc Tue";
+      Console.WriteLine(name);  
+      
+      int myNum =2121050190 ;
+    Console.WriteLine(myNum);
+     }
+  }
+}
