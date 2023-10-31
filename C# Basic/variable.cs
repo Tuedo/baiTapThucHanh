bd08fda310c0f9671c486030abd73e5032d59c93
Tuedo <tuedoduc1111@gmail.com>
2023-10-31 14:50:23 +0700

    khai bao kieu bien

---
+++ C# Basic/variable.cs
@@ -0,0 +1,36 @@
+// ho va ten : Do Duc Tue mssv 2121050190
+
+namespace variable
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Khai báo biến có tên studentName, lưu loại dữ liệu chuỗi
+            string studentName;
+
+            // Khai báo biến và gán ngay giá trị lưu vào biến
+            string address = "Hà Nội, Việt Nam";
+
+            // Khai báo biến, sau đó trước khi sử dụng gán giá trị cho biến
+            int studentAge;
+            studentAge = 22;
+
+            int seconds = 60;                    //khai báo biến số nguyên
+            double so_pi = 3.14;                 //khai báo biến số thực
+            bool deltaIsSezo = true;             //Khai báo biến logic
+            char chooseAction = 'S' ;            //Khai báo biến kiểu ký tự
+            string msgResult = "Kết quả giải:" ; // khai báo biến chuỗi
+
+ 
+            
+            
+
+        }
+
+         
+
+
+    }
+}
