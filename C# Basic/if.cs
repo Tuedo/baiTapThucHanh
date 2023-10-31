cddceab3794e3ee4b2721b06d79880d3bab61adb
Tuedo <tuedoduc1111@gmail.com>
2023-10-31 14:49:36 +0700

    khai bao bien

---
+++ C# Basic/if.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      if (x = 10) 
+      {
+        Console.WriteLine("Do duc tue");
+      } 
+      else if (x <10) 
+      {
+        Console.WriteLine("2121050190");
+      } 
+      else 
+      {
+        Console.WriteLine("Tue dep trai");
+      }
+    }
+  }
+}
