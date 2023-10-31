10efc0f9d9f990577480ac98bee8d9b821d74574
Tuedo <tuedoduc1111@gmail.com>
2023-10-31 23:33:47 +0700

    slide 20 chuong 7

---
+++ DemoMVC/Models/HeThongPhanPhoi.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+namespace DemoMVC.Models
+// 2121050190 - doduc tue
+{
+    public class HeThongPhanPhoi
+    {
+       
+        public string MaHTPP{ get; set; }
+       
+        public string TenHTPP{ get; set; }
+    
+    }
+}
